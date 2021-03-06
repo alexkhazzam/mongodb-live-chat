dcb5c16242b8ad99247944dc567b92347dff5ca9
alexkhazzam <akhazzam1@student.gn.k12.ny.us>
2021-03-05 21:33:50 -0500

    fallback for PORT being occupie

--- server.js
+++ server.js
@@ -4,8 +4,21 @@ const colors = require('colors');
 
 dotenv.config({ path: 'dotenv.config.env' });
 
-const PORT = process.env.PORT || 5000;
+const DEFAULT_PORT = process.env.DEFAULT_PORT || 5000;
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Listening to requests on port ${PORT}`.blue);
-});
+const listener = (PORT_NUMBER) => {
+  console.log(`Listening to requests on port ${PORT_NUMBER}`.blue);
+};
+
+app
+  .listen(DEFAULT_PORT, () => {
+    listener(DEFAULT_PORT);
+  })
+  .on('error', (e) => {
+    if (e.code === 'EADDRINUSE') {
+      app.listen(PORT, () => {
+        listener(PORT);
+      });
+    }
+  });
