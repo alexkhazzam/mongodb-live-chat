40157e115bf00daa2d49b3498736863496c082ee
alexkhazzam <akhazzam1@student.gn.k12.ny.us>
2021-03-06 22:43:42 -0500

    removing async await

--- models/login.js
+++ models/login.js
@@ -2,11 +2,13 @@ const UserSchema = require('../schema/User');
 const bcrypt = require('bcrypt');
 
 module.exports.AuthenticateUser = class {
-  static async fetchEmail(email, callback) {
-    return await UserSchema.findOne({ email: email }).catch((err) => {
-      console.log(`${err}`.red);
-      throw err;
-    });
+  static async fetchEmail(email, user) {
+    user(
+      await UserSchema.findOne({ email: email }).catch((err) => {
+        console.log(`${err}`.red);
+        throw err;
+      })
+    );
   }
 
   static async comparePasswords(user, password) {
