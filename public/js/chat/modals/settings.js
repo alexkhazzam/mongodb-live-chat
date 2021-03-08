1e8a35b6cae3ebc4dbbcd098ebc94c87698ae95b
alexkhazzam <akhazzam1@student.gn.k12.ny.us>
2021-03-08 14:45:27 -0500

    navbar for settings modal

--- public/js/chat/modals/settings.js
+++ public/js/chat/modals/settings.js
@@ -1,10 +1,56 @@
 const settingsLi = document.getElementById('settings-li');
 const html = document.querySelector('html');
+const navLinks = document.querySelectorAll('.nav-link');
+const modalNav = document.getElementById('main-settings-nav');
+const closeModal = document.getElementById('close-modal');
 const settingsModal = document.getElementById('settings-modal');
+
+const preferencesLink = document.getElementById('preferences-link');
+const preferencesContent = document.getElementById('preferences');
+
+const securityLink = document.getElementById('security-link');
+const securityContent = document.getElementById('security');
+
+const adminLink = document.getElementById('admin-link');
+const adminContent = document.getElementById('admin');
+
+adminLink.addEventListener('click', () => {
+  adminContent.style.display = 'block';
+  preferencesContent.style.display = 'none';
+  securityContent.style.display = 'none';
+});
+
+preferencesLink.addEventListener('click', () => {
+  adminContent.style.display = 'none';
+  securityContent.style.display = 'none';
+  preferencesContent.style.display = 'block';
+});
+
+securityLink.addEventListener('click', () => {
+  adminContent.style.display = 'none';
+  preferencesContent.style.display = 'none';
+  securityContent.style.display = 'block';
+});
+
+let clicked = false;
+
+closeModal.addEventListener('click', () => {
+  preferencesLink.className += ' active-link';
+  closeModal.className = 'nav-link';
+  settingsModal.style.display = 'none';
+});
 
 settingsLi.addEventListener('click', () => {
   settingsModal.style.display = 'block';
-  settingsModal.addEventListener('click', () => {
-    settingsModal.style.display = 'none';
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    navLinks.forEach((l) => {
+      if (l.className.includes('active-link')) {
+        l.className = 'nav-link';
+      }
+    });
+    e.target.className += ' active-link';
   });
 });
--- public/js/chat/navbar.js
+++ public/js/chat/navbar.js
@@ -1,6 +1,8 @@
 const scrollUpLi = document.getElementById('scroll-up-li');
 const pageName = document.getElementById('page-name');
+const activePageHeader = document.getElementById('header');
 
 scrollUpLi.addEventListener('click', () => {
+  activePageHeader.scrollIntoView({ behavior: 'smooth' });
   pageName.scrollIntoView({ behavior: 'smooth' });
 });
